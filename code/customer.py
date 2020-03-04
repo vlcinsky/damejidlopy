4d947dd6887c995e22a0da4959f0923608ffdbc9
Daniel Kukucz <daniel@kukucz.com>
2020-03-04 21:05:24 +0100

    Added doc strings, find_info function, Translated hole code into english

---
+++ code/customer.py
@@ -0,0 +1,16 @@
+class Customer:
+	"""Class which stores information about customer
+	Arguments:
+		name(str): Name of the customer,
+		surname(str): Surname of customer,
+		email(str): Email of customer,
+		address(str): Adress of customer
+
+	"""
+	def __init__(self, name, surname, email, address):
+		"""Gets information using arguments and then prints it"""
+		self.name = name
+		self.surname = surname
+		self.email = email
+		self.address = address
+		print(f"Name: {self.name}, Surname: {self.surname}, Email: {self.email}, Address: {self.address}")
