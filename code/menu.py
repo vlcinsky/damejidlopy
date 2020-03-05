91e25004ccc883c467644c21b1b3cc2e246bb46b
Daniel Kukucz <daniel@kukucz.com>
2020-03-05 16:55:25 +0100

    Finished work on menu.py

--- code/menu.py
+++ code/menu.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 
 def menu(searched, restaurant):
-    cislo = 1
+    number_of_food = 1
     _id, driver = searched
     button = driver.find_element_by_xpath(f"//a[@data-id='{_id[restaurant - 1]}']")
     driver.execute_script("arguments[0].click();", button)
@@ -16,24 +16,30 @@ def menu(searched, restaurant):
     except selenium.common.exceptions.TimeoutException:
         print("Webpage couldn't load")
     page = BeautifulSoup(driver.page_source, "lxml")
-    for menicko in page.find_all("div", class_="RestaurantDetailMenuCategory"):
-        textik = menicko.find("h2", class_="RestaurantDetailMenuCategory__title")
+    for category in page.find_all("div", class_="RestaurantDetailMenuCategory"):
+        title = category.find("h2", class_="RestaurantDetailMenuCategory__title")
         title = [
             slovo
-            for slovo in textik.text.split()
-            if slovo not in textik.find(
+            for slovo in title.text.split()
+            if slovo not in title.find(
                 "div", class_="RestaurantDetailMenuCategory__packing"
             ).text.split()
         ]
         title = " ".join(title)
         print(title)
         print("-" * 20)
-        for jidlo in menicko.find_all(class_="RestaurantDetailMenuCategory__list"):
-            for mensijidlo in jidlo.find_all(class_="Menu-list__item-text"):
-                text = mensijidlo.find("a", class_="basketAdd").text.strip().split()
+        for category in category.find_all(class_="RestaurantDetailMenuCategory__list"):
+            for food in category.find_all(class_="Menu-list__item-text"):
+                text = food.find("a", class_="basketAdd").text.split()
                 if text[0][0].isnumeric():
                     text.pop(0)
                 text = " ".join(text)
-                print(f"""{text}
+                print(f"""{number_of_food}. {text}:
+{food.find("p", class_="Menu-list__item-description").text.strip()}
+Price: {food.find("span", class_="Menu-list__item-priceValue").text.strip()}
+    
                 """)
+                number_of_food += 1
             print("-" * 20)
+    driver.back()
+    return button, driver
